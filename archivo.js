131cafb8fc03ddd73e9743a4e53c5c51c6ca11b8
FMenichetti <129986699+FMenichetti@users.noreply.github.com>
2024-05-13 19:41:56 -0300

    carrousel

--- archivo.js
+++ archivo.js
@@ -1,5 +1,15 @@
 
 
 
-let carrousel= [];
+let carrousel= ['./Imagenes/logo.png', './Imagenes/sobreNosotros.jpeg', './Imagenes/web_en_construccion.jpg'];
+let indice = 0;
 
+function cambiarImagen( num ){
+    indice += num;
+    if ( indice < 0 ) { // Si es menor que cero vulvo al final
+        indice = carrousel.length -1;
+    }else if( indice >= carrousel.length ){ // si es mayor que el largo del array, voy a cero
+        indice = 0;
+    }
+    document.getElementById('actual').src = carrousel[indice]; //Cambia el indice de imagen
+}
